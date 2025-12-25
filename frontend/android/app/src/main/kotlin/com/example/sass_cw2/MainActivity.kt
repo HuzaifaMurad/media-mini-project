629666813d28d3ecc23f1f8ebe6957948dcc7b08
HuzaifaMurad <hkexapril@gmail.com>
2025-12-25 17:29:58 +0000

    Remove Flutter frontend, add React frontend, ignore Azurite artifacts

--- frontend/android/app/src/main/kotlin/com/example/sass_cw2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sass_cw2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/lib/api/api_config.dart
+++
@@ -1,9 +0,0 @@
-class ApiConfig {
-  // Local dev (Functions Core Tools)
-  // static const baseUrl = 'http://localhost:7071/api';
-
-  // Deployed (replace later)
-  static const baseUrl = 'https://func-mediamini-sass-cubtdzhpfcandgew.polandcentral-01.azurewebsites.net/api';
-
-  static const isLocalDev = false;
-}
